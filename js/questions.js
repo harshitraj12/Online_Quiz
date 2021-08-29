eefe702494581270546edc33cad848333ab3c738
Harshit Raj <harshitraj4839@gmail.com>
2021-08-29 08:54:56 +0530

    Rules and UI Changed

---
+++ js/questions.js
@@ -0,0 +1,115 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: "Q",
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: "Q",
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: "Q",
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: "Q",
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: "Q",
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+  {
+    numb: "Q",
+    question: "What does AWS stand for?",
+    answer: "Amazon Web Services",
+    options: [
+      "Amazon Web Services",
+      "Apple Web Server",
+      "Amazon Work Services",
+      "Amazon Web Server"
+    ]
+  },
+    {
+      numb: "Q",
+      question: "Which topic is not a part of walkover training?",
+      answer: "Python",
+      options: [
+        "Git",
+        "AWS",
+        "Python",
+        "SQL"
+      ]
+    },
+    {
+      numb: "Q",
+      question: "Extension of JavaScript?",
+      answer: ".js",
+      options: [
+        ".jt",
+        ".js",
+        ".j",
+        ".java"
+      ]
+    },
+    {
+      numb: "Q",
+      question: "Extension of Python is?",
+      answer: ".py",
+      options: [
+        ".python",
+        ".py",
+        ".p",
+        ".pyt"
+      ]
+    },
+    {
+      numb: "Q",
+      question: "Extension of c++ is?",
+      answer: ".cpp",
+      options: [
+        ".c++",
+        ".cpp",
+        ".c",
+        ".cp+"
+      ]
+    },
+];
